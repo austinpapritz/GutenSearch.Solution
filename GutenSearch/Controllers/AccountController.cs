e24e98e6bd0fcf19b4dae3e5186d43bc02f9306e
Austin Papritz <austin@papritz.dev>
2023-08-07 14:16:27 -0700

    added login routes, need to build views
    Co-authored-by: iamalion <iamalion@users.noreply.github.com>
    Co-authored-by: Stella Marie <SmKou@users.noreply.github.com>
    Co-authored-by: Eva Kemp <ekmagiccat@users.noreply.github.com>
    Co-authored-by: Raed Alkhanbashi <rodeomar@users.noreply.github.com>

--- GutenSearch/Controllers/AccountController.cs
+++ GutenSearch/Controllers/AccountController.cs
@@ -28,30 +28,59 @@ namespace GutenSearch.Controllers
             return View();
         }
 
-        // [HttpPost]
-        // public async Task<ActionResult> Register(RegisterViewModel model)
-        // {
-        //     if (!ModelState.IsValid)
-        //     {
-        //         return View(model);
-        //     }
-        //     else
-        //     {
-        //         ApplicationUser user = new ApplicationUser { UserName = model.Email };
-        //         IdentityResult result = await _userManager.CreateAsync(user, model.Password);
-        //         if (result.Succeeded)
-        //         {
-        //             return RedirectToAction("Index");
-        //         }
-        //         else
-        //         {
-        //             foreach (IdentityError error in result.Errors)
-        //             {
-        //                 ModelState.AddModelError("", error.Description);
-        //             }
-        //             return View(model);
-        //         }
-        //     }
-        // }
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                ApplicationUser user = new ApplicationUser { UserName = model.Email };
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View(model);
+                }
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Something went wrong when logging in. Please try again.");
+                    return View(model);
+                }
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> LogOff()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index");
+        }
     }
 }
