e94f2024750fc24cdc27f0651c04acf3be91c4a3
Austin Papritz <austin@papritz.dev>
2023-08-08 13:48:48 -0700

    added AuthorBook list to Author.cs

--- GutenSearch/Models/Author.cs
+++ GutenSearch/Models/Author.cs
@@ -8,4 +8,6 @@ public class Author
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string FullName => $"{FirstName} {LastName}";
+
+    public List<AuthorBook> AuthorBooks { get; set; }
 }
